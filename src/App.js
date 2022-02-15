de03e75b4b8b29d44b6afb816514a9cd5774c5de
abdulrahmanshr75 <94563329+abdulrahmanshr75@users.noreply.github.com>
2022-02-15 09:25:02 +0200

    setup calculator skeleton

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Calculator from "./components/calculator.js";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
 }
 
 export default App;
